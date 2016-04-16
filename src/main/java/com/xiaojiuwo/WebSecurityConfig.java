be5d70328142ba5b16b73640eacfc7a83a4c5fbe
Soar <85624529@qq.com>
2016-04-16 13:48:11 +0800

    update version and remove some useless

--- src/main/java/com/xiaojiuwo/WebSecurityConfig.java
+++ src/main/java/com/xiaojiuwo/WebSecurityConfig.java
@@ -1,15 +1,9 @@
 package com.xiaojiuwo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 /**
  * Created by liuhaibao on 16/1/18.
  */
+/*
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,7 +11,7 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home","/*/**","/*").permitAll()
+                .antMatchers("/", "/home",").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -34,4 +28,4 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
-}
+}*/
