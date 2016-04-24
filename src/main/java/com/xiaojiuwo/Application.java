df1d09433442e816d0c04935be7247efc824800c
Soar <85624529@qq.com>
2016-04-24 20:52:20 +0800

    [feature] add turbine

--- src/main/java/com/xiaojiuwo/Application.java
+++ src/main/java/com/xiaojiuwo/Application.java
@@ -6,12 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableHystrixDashboard
+@EnableTurbine
 @EnableDiscoveryClient
+
+
 public class Application {
 
     public static void main(String[] args) {
