5211453795e49b2953810fc7d079b7f2f7bef3e5
Soar <85624529@qq.com>
2016-04-29 18:48:47 +0800

    [feature] add zuul

--- src/main/java/com/xiaojiuwo/Application.java
+++ src/main/java/com/xiaojiuwo/Application.java
@@ -1,5 +1,6 @@
 package com.xiaojiuwo;
 
+import com.xiaojiuwo.filters.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -14,8 +17,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableHystrixDashboard
 @EnableTurbine
 @EnableDiscoveryClient
-
-
+@EnableZuulProxy
 public class Application {
 
     public static void main(String[] args) {
@@ -24,6 +26,11 @@ public class Application {
         springApplication.addListeners(new ApplicationPidFileWriter());
         springApplication.run(args);
 
+    }
+
+    @Bean
+    public AuthFilter authFilter() {
+        return new AuthFilter();
     }
 }
 
